5f9b9a801351cb38a4e2e8e4e20e1d6ba9f0c726
mjs2020 <me@fm.to.it>
2015-02-09 17:49:26 +0000

    Added partnerAreas json too

--- scripts/helpers/controls.js
+++ scripts/helpers/controls.js
@@ -10,29 +10,19 @@
 
 
 define(function() {
-  //return an object to define the "my/shirt" module.
-  return {
-    setup: function() {
-
+  var controls = {
+    setup: function () {
       'use strict';
 
-      // Setup loader overlay
-      // Init spinner
+      // SETUP LOADER OVERLAY
       var $loading = $('#loadingDiv').hide();
-      $(document)
-        .ajaxStart(function () {
-          $loading.show();
-        })
-        .ajaxStop(function () {
-          $loading.hide();
-        });
+      $(document).ajaxStart(function () { $loading.show(); }).ajaxStop(function () { $loading.hide(); });
 
       // SETUP SELECT2 DROPDOWNS SELECTORS
-      // Load reporters and partner lists from JSON (from local JSON file currently)
+      // Load reporters list from JSON (from local JSON file currently)
       $.ajax('/data/reporterAreas.min.json', {
-        crossDomain: false,
         error: function (xhr, status, error) {
-          console.log('Request for partner/reporter list failed with status: '+status+' and error: '+error);
+          console.log('Request for partner list failed with status: '+status+' and error: '+error);
         },
         success: function (response, status, xhr) {
           $("#selectReporter").select2({
@@ -40,14 +30,25 @@ define(function() {
             allowClear: true,
             data: response.results
           });
+          $("#selectReporter").on('change', controls.onFilterChange);
+        }
+      });
+      // Load reporters list from JSON (from local JSON file currently)
+      $.ajax('/data/partnerAreas.min.json', {
+        error: function (xhr, status, error) {
+          console.log('Request for partner/reporter list failed with status: '+status+' and error: '+error);
+        },
+        success: function (response, status, xhr) {
           $("#selectPartner").select2({
             placeholder: "Select a partner",
             allowClear: true,
+            disabled: true,
             data: response.results
           });
+          $("#selectPartner").on('change', controls.onFilterChange);
+          $("#selectPartner").select2('disable');
         }
       });
-
       // Load commodities list (from local JSON file currently)
       $.ajax('/data/classificationHS_AG2.min.json', {
         crossDomain: false,
@@ -60,24 +61,57 @@ define(function() {
             allowClear: true,
             data: response.results
           });
+          $("#selectCommodity").on('change', controls.onFilterChange);
+          $("#selectCommodity").select2('disable');
         }
       });
 
-      // Add import/export/balance button behaviours
+      // ADD IMPORT/EXPORT/BALANCE BUTTON BEHAVIOURS
       $('#flowButtons').click(function (evt) {
         $('#flowButtons button').removeClass('btn-primary').addClass('btn-default');
         $(event.target).closest('button').removeClass('btn-default').addClass('btn-primary');
         console.log('Selected '+$(event.target).closest('button').html());
       });
 
-      // Add clearFilters button behavior
+      // ADD CLEARFILTERS BUTTON BEHAVIOR
       $("#clearFilters").click(function (evt) {
-        $('#selectCommodity').val(null).trigger("change");
-        $('#selectReporter').val(null).trigger("change");
-        $('#selectPartner').val(null).trigger("change");
+        $('.select2control')
+          .off('change', controls.onFilterChange)
+          .val(null)
+          .trigger("change")
+          .on('change', controls.onFilterChange);
+        controls.onFilterChange();
       });
+    },
+
+    onFilterChange: function (evt) {
+      'use strict';
+
+      // Check what changed
+      var reporter = $('#selectReporter').val(),
+          partner = $('#selectPartner').val(),
+          commodity = $('#selectCommodity').val();
+
+      if(!reporter) {
+        $('.select2control')
+          .off('change', controls.onFilterChange)
+          .val(null)
+          .trigger("change")
+          .on('change', controls.onFilterChange);
+        $("#selectCommodity").select2('disable');
+        $("#selectPartner").select2('disable');
+      } else {
+        $("#selectCommodity").select2('enable');
+        $("#selectPartner").select2('enable');
+      }
+
+
+      console.log('Filters changed!');
     }
+
   }
+
+  return controls;
 });
 
 
