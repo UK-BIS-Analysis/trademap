456f53e9134129cf2d69c95d7e55430faa22445c
mjs2020 <me@fm.to.it>
2015-02-13 15:05:14 +0000

    Created placeholders for each chart.

---
+++ scripts/helpers/charts.js
@@ -0,0 +1,23 @@
+/*jslint browser: true*/
+/*jslint white: true */
+/*jslint vars: true */
+/*global $, Modernizr, d3, dc, crossfilter, document, console, alert, define, DEBUG */
+
+
+/*
+ * THIS FILE SETS UP EACH OF THE CHARTS
+ * */
+
+
+define(function() {
+  'use strict';
+
+  var charts = {
+    setup: function () {
+
+    }
+
+  };
+
+  return charts;
+});
---
+++ scripts/helpers/charts/topExportDestinations.js
@@ -0,0 +1,23 @@
+/*jslint browser: true*/
+/*jslint white: true */
+/*jslint vars: true */
+/*global $, Modernizr, d3, dc, crossfilter, document, console, alert, define, DEBUG */
+
+
+/*
+ * THIS FILE SETS UP THE topExportDestinations chart
+ * */
+
+
+define(function() {
+  'use strict';
+
+  var chart = {
+    setup: function () {
+
+    }
+
+  };
+
+  return chart;
+});
