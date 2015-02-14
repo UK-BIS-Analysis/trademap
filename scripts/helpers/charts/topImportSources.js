f6369962110955eb00264a0509966924136b6f17
mjs2020 <me@fm.to.it>
2015-02-14 22:56:20 +0000

    Completed topImportSources.js

--- scripts/helpers/charts/topImportSources.js
+++ scripts/helpers/charts/topImportSources.js
@@ -9,22 +9,82 @@
  * */
 
 
-define(function() {
+define(['../data'], function(data) {
   'use strict';
 
-  var chart = {
+  var localData = data,
+      $chart = $('#topImportSources'),
+      chart = {
 
-    setup: function () {
-      // Initialize chart state:
-      $('#topImportSources .placeholder').html('');
+        setup: function () {
+          $chart
+            .on('refreshFilters', this.refresh)
+            .slideUp(0)
+            .children('.placeholder')
+            .html('');
+        },
 
-      // Add filterUpdate listener
-      $('#topImportSources').on('refreshFilters', chart.refresh);
-    },
+        refresh: function (event, filters) {
+          // CASE 1: reporter = null
+          if(!filters.reporter) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
 
-    refresh: function (event, filters) {
+          // CASE 2: reporter = selected    commodity = null        partner = null
+          if(filters.reporter && !filters.commodity && !filters.partner) {
+            data.query({
+              reporter: filters.reporter,
+              period:   filters.year,
+              hsCode:   'AG2'
+            }, function queryCallback (err, data) {
+              // TODO: do something here
+              $chart
+                .slideDown()
+                .children('.placeholder')
+                .html('Bar chart of top 20 import sources for '+localData.reporterAreas[filters.reporter]+' in '+filters.year);
+            });
+            return;
+          }
 
-    }
+          // CASE 3: reporter = selected    commodity = null        partner = selected
+          if(filters.reporter && !filters.commodity && filters.partner) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
+
+          // CASE 4: reporter = selected    commodity = selected    partner = null
+          if(filters.reporter && filters.commodity && !filters.partner) {
+            data.query({
+              reporter: filters.reporter,
+              period:   filters.year,
+              partner:  'all',
+              hsCode:   filters.commodity
+            }, function queryCallback (err, data) {
+              // TODO: do something here
+              $chart
+                .slideDown()
+                .children('.placeholder')
+                .html('Bar chart of top 20 import sources of '+localData.classificationCodes[filters.commodity]+' for '+localData.reporterAreas[filters.reporter]+' in '+filters.year);
+            });
+            return;
+          }
+
+          // CASE 5: reporter = selected    commodity = selected    partner = selected
+          if(filters.reporter && filters.commodity && filters.partner) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
+        }
 
   };
 
