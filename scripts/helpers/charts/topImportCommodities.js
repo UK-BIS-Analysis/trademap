b9e92e95c4b16ba6d1047d73f5261f25e189383e
mjs2020 <me@fm.to.it>
2015-02-14 23:18:05 +0000

    Completed topImportCommodities.js

--- scripts/helpers/charts/topImportCommodities.js
+++ scripts/helpers/charts/topImportCommodities.js
@@ -9,24 +9,82 @@
  * */
 
 
-define(function() {
+define(['../data'], function(data) {
   'use strict';
 
-  var chart = {
+  var localData = data,
+      $chart = $('#topImportCommodities'),
+      chart = {
 
-    setup: function () {
-      // Initialize chart state:
-      $('#topImportCommodities .placeholder').html('');
+        setup: function () {
+          $chart
+            .on('refreshFilters', this.refresh)
+            .slideUp(0)
+            .children('.placeholder')
+            .html('');
+        },
 
-      // Add filterUpdate listener
-      $('#topImportCommodities').on('refreshFilters', chart.refresh);
-    },
+        refresh: function (event, filters) {
+          // CASE 1: reporter = null
+          if(!filters.reporter) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
 
-    refresh: function (event, filters) {
+          // CASE 2: reporter = selected    commodity = null        partner = null
+          if(filters.reporter && !filters.commodity && !filters.partner) {
+            data.query({
+              reporter: filters.reporter,
+              period:   filters.year,
+              hsCode:   'AG2'
+            }, function queryCallback (err, data) {
+              // TODO: do something here
+              $chart
+                .slideDown()
+                .children('.placeholder')
+                .html('Bar chart with top 20 import commodities in '+filters.year+' for '+localData.reporterAreas[filters.reporter]+' from rest of the world.');
+            });
+            return;
+          }
 
-    }
+          // CASE 3: reporter = selected    commodity = null        partner = selected
+          if(filters.reporter && !filters.commodity && filters.partner) {
+            data.query({
+              reporter: filters.reporter,
+              period:   'all',
+              partner:  filters.partner,
+              hsCode:   'AG2'
+            }, function queryCallback (err, data) {
+              // TODO: do something here
+              $chart
+                .slideDown()
+                .children('.placeholder')
+                .html('Bar chart with top 20 import commodities in '+filters.year+' for '+localData.reporterAreas[filters.reporter]+' from '+localData.partnerAreas[filters.partner]+'.');
+            });
+            return;
+          }
 
-  };
+          // CASE 4: reporter = selected    commodity = selected    partner = null
+          if(filters.reporter && filters.commodity && !filters.partner) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
 
+          // CASE 5: reporter = selected    commodity = selected    partner = selected
+          if(filters.reporter && filters.commodity && filters.partner) {
+            $chart
+              .slideUp()
+              .children('.placeholder')
+              .html('');
+            return;
+          }
+        }
+      };
   return chart;
 });
