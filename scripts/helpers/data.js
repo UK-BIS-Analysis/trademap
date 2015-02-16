215d1836bf8cffa7dca9848f3da48ee919c8c584
mjs2020 <me@fm.to.it>
2015-02-16 16:23:48 +0000

    Adding of loaded data into crossfilter.

--- scripts/helpers/data.js
+++ scripts/helpers/data.js
@@ -11,7 +11,8 @@
 define(function(require) {
   'use strict';
 
-  // Using require above we are making data a singleton which is created only once. Each module requiring data will be using the same object.
+  // Using require above we are making data a singleton which is created only once.
+  // Each module requiring data will be using the same object.
   var singleton = function () {
     var data = {
 
@@ -19,28 +20,26 @@ define(function(require) {
        * PROPERTIES
        * Some basic properties that we store and persist throughout the application
        */
-
       // Base query url
       baseQueryUrl: 'http://comtrade.un.org/api/get?fmt=csv&max=50000&type=C&freq=A&px=HS&rg=1%2C2',
 
-      // Query history will be an array of query urls that we will consult before running each query
+      // queryHistory, queryQueue and timestamp are used to throttle and debounce queries
       queryHistory: [],
       queryQueue: [],
-
-      // We store the time we fire every call here in order to delay calls to fire no more than one per second.
       timestamp: 0,
 
-      // reporter, partner and classification arrays for select2 widgets
+      // Reporter, partner and classification arrays for select2 widgets and lookup objects
+      // These are populated during controls setup with data from
+      // reporterAreas.json, partnerAreas.json and clasificationsHS_AG2.json
       reporterAreasSelect: [],
       partnerAreasSelect: [],
       classificationCodesSelect: [],
-      // reporter, partner and classification lookup objects. These are populated during controls creation with data from the reporterAreas.json, partnerAreas.json and clasificationsHS_AG2.json
       reporterAreas: {},
       partnerAreas: {},
       classificationCodes: {},
 
-
-
+      // Crossfilter data
+      ndx: crossfilter(),
 
 
 
@@ -48,7 +47,7 @@ define(function(require) {
 
 
       /*
-       * METHODS
+       * PUBLIC METHODS
        * */
 
 
@@ -88,24 +87,19 @@ define(function(require) {
        */
       query: function (options, callback) {
         // Get current time and build URL
-        var requestUrl = data.baseQueryUrl,
+        var requestUrl = this._buildUrl(options),
             time = new Date();
-        if (options.reporter) { requestUrl += '&r=' +options.reporter; } else { requestUrl += '&r=0'; }
-        if (options.partner)  { requestUrl += '&p=' +options.partner;  } else { requestUrl += '&p=all'; }
-        if (options.period)   { requestUrl += '&ps='+options.period;   } else { requestUrl += '&ps=now'; }
-        if (options.hsCode)   { requestUrl += '&cc='+options.hsCode;   } else { requestUrl += '&cc=AG2'; }
 
         // Check history to see if query was already run and skip the call if it was already run
         if(data.queryHistory.indexOf(requestUrl) > -1) {
-          if (DEBUG) { console.log(time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+': Skipping call:'+requestUrl); }
           callback(null, false);
           return;
         }
 
-        // If the API was called less than a second ago, or if the query is in the queue then we need to postpone the call by (a little more than) a second.
+        // If the API was called less than a second ago, or if the query is in the queue then we need to
+        // postpone the call by (a little more than) a second.
         if (time.getTime() - data.timestamp < 1000 || data.queryQueue.indexOf(requestUrl) > -1) {
-          if (DEBUG) { console.log(time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+': Delaying call: '+requestUrl); }
-          var timeoutID = window.setTimeout(function () { data.query(options, callback); }, 1100);
+          window.setTimeout(function () { data.query(options, callback); }, 1100);
           callback(null, false);
           return;
         }
@@ -114,33 +108,52 @@ define(function(require) {
         $.ajax({
           url: requestUrl,
           crossDomain: true,
-          context: this,    // NOTE: This is imporant as it binds the callback to the data object we are creating. Otherwise we cannot access any of the properties in the callback.
+          // NOTE: context setting is imporant as it binds the callback to the data object we are creating.
+          // Otherwise we cannot access any of the properties in the callback.
+          context: this,
           beforeSend: function (xhr, settings) {
-            if (DEBUG) { console.log(time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+': Making call  : '+requestUrl); }
             // Set the timestamp so that other queries will queue and add the current query to the queue.
             data.timestamp = time.getTime();
             this.queryQueue.push(requestUrl);
           },
           success: function success (data, status, xhr) {
-            // Add query to history
+            // Add query to history and remove it from queryQueue if it was there
             this.queryHistory.push(requestUrl);
-            // Remove query from queue if it was there:
             var queueItem = this.queryQueue.indexOf(requestUrl);
             if (queueItem > -1) { this.queryQueue.splice(queueItem, 1); }
-            if (DEBUG) { console.log(time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+': Call success : '+requestUrl); }
-
-            // TODO: Add data to crossfilter?
-
+            // Add data to crossfilter and callback
+            this._addData(data);
             callback(null, data);
           },
           error: function error (xhr, status, err) {
-            if (DEBUG) { console.log(time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+': Request failed: '+requestUrl+' with status: '+status); }
             callback(err, null);
           }
         });
+      },
+
+
+
+
+
+
+
+      /*
+       * PRIVATE METHODS
+       * (methods that are only used internally in the data module)
+       * */
+      _buildUrl: function (options) {
+        var requestUrl = data.baseQueryUrl;
+        if (options.reporter) { requestUrl += '&r=' +options.reporter; } else { requestUrl += '&r=0'; }
+        if (options.partner)  { requestUrl += '&p=' +options.partner;  } else { requestUrl += '&p=all'; }
+        if (options.period)   { requestUrl += '&ps='+options.period;   } else { requestUrl += '&ps=now'; }
+        if (options.hsCode)   { requestUrl += '&cc='+options.hsCode;   } else { requestUrl += '&cc=AG2'; }
+        return requestUrl;
+      },
+
+      _addData: function (csvData) {
+        var newData = d3.csv.parse(csvData);
+        this.ndx.add(newData);
       }
-
-
 
     };
     return data;
