005508b853ff45e7507cc8db3a834986bcb338da
mjs2020 <me@fm.to.it>
2015-04-28 09:43:40 +0100

    Changed colors and increased flowbutton size

--- scripts/helpers/charts.js
+++ scripts/helpers/charts.js
@@ -19,7 +19,7 @@ define(['./charts/choropleth', './charts/yearChart', './charts/infoBox', './char
     // Color schemes from http://colorbrewer2.org/
     // http://colorbrewer2.org/?type=sequential&scheme=YlGn&n=5
     // The number of colors will drive the scales below (e.g. if you put six colors there will be six shades in the scales)
-    balanceColors : ['rgb(191, 25, 0)','rgb(120,198,121)'],// import/export - red/green
+    balanceColors : ['rgb(8,104,172)','rgb(120,198,121)'],// import/export - blue/green
     importColors  : ['rgb(240,249,232)','rgb(186,228,188)','rgb(123,204,196)','rgb(67,162,202)','rgb(8,104,172)'],  // blues
     exportColors  : ['rgb(255,255,204)','rgb(194,230,153)','rgb(120,198,121)','rgb(49,163,84)','rgb(0,104,55)'], // greens
 
